bdb57795248efe36edbd78998b75530368e412b2
Dasha Nikitina <dashadariallc@gmail.com>
2017-12-22 05:23:57 -0500

    added login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
-  
+
+  resources :users, only: [:show] do
+    resources :workouts
+  end
+
+  resources :exercises
+
+  get '/signup' to: 'users#new'
+  post '/signup'to: 'users#create'
+
+  get '/login' to: 'sessions#new'
+  post '/login' to: 'sessions#create'
+  delete '/logout' to: 'sessions#destroy'
+
+
 end
