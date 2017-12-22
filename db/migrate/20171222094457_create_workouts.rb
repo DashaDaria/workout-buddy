0f22f500c5c676ac1da9e81c61622179db1f4ff1
Dasha Nikitina <dashadariallc@gmail.com>
2017-12-22 05:09:56 -0500

    added migrations

--- db/migrate/20171222094457_create_workouts.rb
+++ db/migrate/20171222094457_create_workouts.rb
@@ -1,6 +1,9 @@
 class CreateWorkouts < ActiveRecord::Migration[5.1]
   def change
     create_table :workouts do |t|
+      t.string :name
+      t.string :status, default: "not completed"
+      t.integer :user_id
 
       t.timestamps
     end
--- db/migrate/20171222094506_create_exercises.rb
+++ db/migrate/20171222094506_create_exercises.rb
@@ -1,6 +1,11 @@
 class CreateExercises < ActiveRecord::Migration[5.1]
   def change
     create_table :exercises do |t|
+      t.string :name
+      t.integer :category_id
+      t.integer :difficulty
+      t.text :how
+      t.insteger :length
 
       t.timestamps
     end
