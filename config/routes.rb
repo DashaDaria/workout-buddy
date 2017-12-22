2ef4a869cf11358befddc60a899b0d10998b68d9
Dasha Nikitina <dashadariallc@gmail.com>
2017-12-22 05:25:39 -0500

    revised routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,12 @@ Rails.application.routes.draw do
 
   resources :exercises
 
-  get '/signup' to: 'users#new'
-  post '/signup'to: 'users#create'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
-  get '/login' to: 'sessions#new'
-  post '/login' to: 'sessions#create'
-  delete '/logout' to: 'sessions#destroy'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
 
 end
