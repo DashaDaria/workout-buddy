e87f4f1ade47321daac157faf7efcfbd7c3f45c0
Dasha Nikitina <dashadariallc@gmail.com>
2017-12-23 00:25:31 -0500

    routed the signup form:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  def show
+    if current_user
+      @user = User.new
+    else
+      redirect_to signup_path
+    end
+  end
+
   def new
     @user = User.new
   end
@@ -7,8 +15,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Thank you for signing up!"
-      redirect_to user_workouts_path
+      redirect_to user_workouts_path(@user)
     else
       flash[:alert] = "Input not valid"
       redirect_to signup_path
