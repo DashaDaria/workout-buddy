cf4f33f53b74fb25eb289cd3b2dd1314efdb4032
Dasha Nikitina <dashadariallc@gmail.com>
2017-12-23 01:13:17 -0500

    creating login with sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_workouts_path(@user)
+    else
+      flash[:alert] = "Can't find user"
+      redirect_to signup_path
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to login_path
+  end
+
 end
