93b77a8b017d4ac1ae48ef5fad0cf9f388fb7f56
Dasha Nikitina <dashadariallc@gmail.com>
2017-12-23 01:44:24 -0500

    added helper methods to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     # raise params.inspect
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
+      log_in @user
       redirect_to user_workouts_path(@user)
     else
       flash.now[:danger] = "Invalid email/password combination"
