71d95549ce9e6f2d443c96d8dcc01046b67983af
Dasha Nikitina <dashadariallc@gmail.com>
2017-12-24 00:38:42 -0500

    added field with errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :workouts
-  validates :name, presence: { message: "Please enter your name."}
-  validates :email, presence: { message: "Email cannot be blank."}
+  validates :name, presence: true
+  validates :email, presence: true
   validates :email, uniqueness: { message: "This email has already been used."}
 end
