7ba2b78d0bd8ec5252f78e30eeba2c26a8db2b5e
Dasha Nikitina <dashadariallc@gmail.com>
2017-12-24 01:36:32 -0500

    added workout methods

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,4 +2,16 @@ class Workout < ApplicationRecord
   belongs_to :user
   has_many :workout_exercises
   has_many :exercises, through: :workout_exercises
+
+  def intensity
+    level = self.exercises.map {|e| e.difficulty }.sum
+    if level <= 3
+      "easy"
+    elsif level >= 9
+      "hard"
+    else
+      "moderate"
+    end
+  end
+
 end
