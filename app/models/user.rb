3d2d4afce13980161e7b84fe1c8c8d40620bcd4c
Dasha Nikitina <dashadariallc@gmail.com>
2017-12-23 22:44:46 -0500

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :workouts
+  validates :name, presence: { message: "Please enter your name."}
+  validates :email, presence: { message: "Email cannot be blank."}
+  validates :email, uniqueness: { message: "This email has already been used."}
 end
