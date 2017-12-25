4802bea52848548f413b504fd04aff0cbe44f414
Dasha Nikitina <dashadariallc@gmail.com>
2017-12-25 04:28:52 -0500

    workout new form posting

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -11,15 +11,20 @@ class WorkoutsController < ApplicationController
   end
 
   def create
-
-    @workout = Workout.create(workout_params)
-    redirect_to user_workouts_path(current_user)
+    @user = current_user
+    @workout = Workout.new(workout_params)
+    @workout.user_id = @user.id
+    if @workout.save
+    redirect_to user_workouts_path
+  else
+    render 'new'
+  end
   end
 
 
 private
   def workout_params
-  params.require(:workout).permit(:name,:status)
+  params.require(:workout).permit(:name)
   end
 
 end
