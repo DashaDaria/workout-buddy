1e7d86397fef5c6554ae1205ca0bfed89840829b
Dasha Nikitina <dashadariallc@gmail.com>
2017-12-25 05:38:46 -0500

    workign on edit form

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -21,10 +21,34 @@ class WorkoutsController < ApplicationController
     if @workout.save
       redirect_to user_workouts_path
     else
+      flash[:notice] = "cannot save, try again"
       render 'new'
     end
   end
 
+  def edit
+    @workout = Workout.find(params[:id])
+    @exercises = @workout.exercises
+  end
+
+  def update
+    @workout = Workout.new(workout_params)
+    binding.pry
+    @workout.user_id = current_user.id
+    if @workout.save
+      redirect_to user_workouts_path
+    else
+      flash[:notice] = "cannot save, try again"
+      render 'new'
+    end
+  end
+
+  def destroy
+    @workout = Workout.find(params[:id])
+    @workout.destroy
+    flash[:notice] = "successfully deleted"
+    redirect_to user_workouts_path
+  end
 
 private
   def workout_params
