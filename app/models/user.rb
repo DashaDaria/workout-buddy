4d01cf2874d520861c3059042d450a874f6d62e9
Dasha Nikitina <dashadariallc@gmail.com>
2017-12-26 19:58:31 -0500

    changed fav exercise method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,18 @@ class User < ApplicationRecord
   has_many :workouts
   validates :name, presence: true
   validates :email, presence: true
-  validates :email, uniqueness: { message: "This email has already been used."}
+  validates :email, uniqueness: { message: "has already been used"}
+
+  def favorite_exercise
+    if !self.workouts.empty?
+      e_array = self.workouts.all.map do |workout|
+              workout.exercises.map do |e|
+                e.name
+              end
+            end
+            e_array.max_by {|e| e_array.count(e)}.join(", ")
+    else
+      "no favorites yet!"
+    end
+  end
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -4,20 +4,15 @@ class Workout < ApplicationRecord
   has_many :exercises, through: :workout_exercises
   validates :name, presence: true
 
+  def self.filter_difficulty(level)
+    # if easy - return -> easy scope method
+  end
+
   def exercises_attributes=(e_hashes)
     e_hashes.values.each do |e_attributes|
     exercise = Exercise.find_or_create_by(e_attributes)
     self.workout_exercises.build(:exercise => exercise)
     end
-  end
-
-  def self.favorite_exercise
-    e_array = Workout.all.map do |workout|
-              workout.exercises.map do |e|
-                e.name
-              end
-            end
-    e_array.max_by {|e| e_array.count(e)}.join(', ')
   end
 
   def status
