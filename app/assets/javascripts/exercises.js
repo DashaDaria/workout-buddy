f6a906736521a6bcf2b755932554634365d5cefd
DashaDaria <dashadariallc@gmail.com>
2018-02-09 17:23:51 +0700

    added new exercise to show page

---
+++ app/assets/javascripts/exercises.js
@@ -0,0 +1,14 @@
+
+$(function () {
+  $(".js-nextE").on("click", function() {
+    var nextId = parseInt($(".js-nextE").attr("data-id")) + 1;
+    $.get("/exercises/" + nextId + ".json", function(data) {
+      $(".eName").text(data["name"]);
+      $(".length").text(data["length"]);
+      $(".how").text(data["how"]);
+      $(".category").text(data["category"]["name"]);
+
+      $(".js-nextE").attr("data-id", data["id"]);
+    });
+  });
+});
