23a792599de7834f2f361438e47e5696773c2596
DashaDaria <dashadariallc@gmail.com>
2018-02-12 15:59:14 +0700

    added an exercise row to workouts table

--- app/assets/javascripts/workout.js
+++ app/assets/javascripts/workout.js
@@ -6,14 +6,32 @@ $(function () {
     var values = $(this).serialize();
     var posting = $.post("/workouts/" + workout_id + "/exercises", values);
 
-    posting.done(function(response){
-      exerciseRowHTML = HandlebarsTemplates['exercise_row']({
-        exercise: response
+    posting.done(function(data){
+      var exercise = data
+      var trHTML = '';
+      trHTML +=
+      '<tr><td>'
+      + `<a href="/exercises/${exercise.id}">`
+      + exercise.name
+      + '</a>'
+      + '</td><td>'
+      + exercise.category
+      + '</td><td>'
+      + exercise.level
+      + '</td><td>'
+      + exercise.length
+      + '</td><td>'
+      + exercise.reps
+      + '</td><td>'
+      + exercise.how
+      + '</td></tr>'
+
+      $('#js-exercisesTable tr:last').after(trHTML);
+      $(".add_exercise").html("");
       });
-      $("#js-exercisesTable tr:last").after(exerciseRowHTML);
     });
   });
-});
+
 
 
 ////jQUERY FOR MORE TEXT FOR EXERCISE INSTRUCTIONS (HOW)////
